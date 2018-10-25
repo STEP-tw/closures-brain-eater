d4f00f96c2a30f2f862c02616848c363f7fc06d2
Tilak Puli <34330361+brain-eater@users.noreply.github.com>
2018-10-25 14:48:50 +0530

    Refactor makeFiboGenerator

--- src/lib.js
+++ src/lib.js
@@ -16,46 +16,39 @@ const makeDeltaTracker = function(start){
   let deltaValues ={old:start,delta:0,new:start};
   return function(delta){ 
     if(delta){
-    deltaValues.old = deltaValues.new;
-    deltaValues.delta = delta; 
-    deltaValues.new = deltaValues.old + delta;
-    return deltaValues;
+      deltaValues.old = deltaValues.new;
+      deltaValues.delta = delta; 
+      deltaValues.new = deltaValues.old + delta;
+      return deltaValues;
     }
     deltaValues.new = deltaValues.old;
     deltaValues.delta = 0;
     return deltaValues;
   }
 };
-const makeFiboGenerator = function(lastNum,NumBeforeLast){
-  let currentNumber;
-  let sNo = 1;
-  if(!lastNum){
-     lastNum = 1;
-  }
- if(NumBeforeLast || NumBeforeLast == 0)
-  {
-    let lastNumber = lastNum;
-    lastNum = NumBeforeLast;
-    NumBeforeLast = lastNumber;
-  }
+
+const makeFiboGenerator = function(NumBeforeLast,lastNum){
   if(!NumBeforeLast){
     NumBeforeLast = 0;
+    lastNum = 1;
   }
+  if(!lastNum){
+    lastNum = NumBeforeLast;
+    NumBeforeLast = 0;
+  }
+  let sno = 1;
   return function(){
-    if(sNo == 2){
-      currentNumber = lastNum;
-      sNo++;
-      return currentNumber;
+    if(sno == 1){
+      sno++;
+      return NumBeforeLast;
     }
-    if(sNo == 1){
-      currentNumber = NumBeforeLast;
-      sNo++;
-      return currentNumber;
+    if(sno == 2){
+      sno++;
+      return lastNum;
     }
-    currentNumber = lastNum + NumBeforeLast;
+    let currentNumber = lastNum + NumBeforeLast;
     NumBeforeLast = lastNum;
     lastNum = currentNumber;
-    sNo++;
     return currentNumber;
   }
 };
